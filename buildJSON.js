50d72652017966f6dd7d2db9fe90498b6a8b411b
Gamaliel Obinyan <gamalielobinyan@Gamaliels-MacBook-Pro.local>
2021-01-17 15:52:46 -0500

    Standard structure for contentful JSON

---
+++ buildJSON.js
@@ -0,0 +1,77 @@
+function buildRestoJSON(attribute, value) {
+    let fields = 
+            {
+                "name": {
+                    "en-US": ""
+                        },
+                "image": {
+                    "en-US": {
+                        "sys": {
+                            "type": "",
+                            "linkType": "",
+                            "id": ""
+                        }
+                    }
+                },
+                "description": {
+                    "en-US": ""
+                },
+                "hasDelivery": {
+                    "en-US": false
+                },
+                "hasPickup": {
+                    "en-US": false
+                },
+                "link": {
+                    "en-US": ""
+                },
+                "location": {
+                    "en-US": {
+                        "lon": 0,
+                        "lat": 0,
+                    }
+                },
+                "tags": {
+                    "en-US": [
+                      ""
+                    ]
+                  },
+                  "deliveryHours": {
+                    "en-US": [
+                      {
+                        "Friday": {
+                          "open": "",
+                          "closed": ""
+                        },
+                        "Monday": {
+                          "open": "",
+                          "closed": ""
+                        },
+                        "Sunday": {
+                          "open": "",
+                          "closed": ""
+                        },
+                        "Tuesday": {
+                          "open": "",
+                          "closed": ""
+                        },
+                        "Saturday": {
+                          "open": "",
+                          "closed": ""
+                        },
+                        "Thursday": {
+                          "open": "",
+                          "closed": ""
+                        },
+                        "Wednesday": {
+                          "open": "1",
+                          "closed": ""
+                        }
+                      }
+                    ]
+                }
+            }       
+}
+    
+
+
