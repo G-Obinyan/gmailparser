a3c03a4b3ccc67797ab8ba4e63d4703134b7d821
Gamaliel Obinyan <gamalielobinyan@Gamaliels-MacBook-Pro.local>
2021-01-21 21:01:59 -0500

    First version of time converter function

---
+++ timeConverter.js
@@ -0,0 +1,13 @@
+function timeConvertor(time) {
+  var PM = time.match("PM") ? true : false;
+  time = time.split(":");
+  var min = time[1];
+  if (PM) {
+    var hour = 12 + parseInt(time[0], 10);
+    var sec = time[2].replace("PM", "");
+  } else {
+    var hour = time[0];
+    var sec = time[2].replace("AM", "");
+  }
+  console.log(hour + ":" + min + ":" + sec);
+}
